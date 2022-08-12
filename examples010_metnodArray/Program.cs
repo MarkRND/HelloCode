a952258f921ca3ec784203daa718127ad2f9869d
Mark <masmik@rambler.ru>
2022-08-12 23:16:06 +0300

    add examples010_metnodArray

---
+++ examples010_metnodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {12,85,3,46,56,61,7,85};
+
+int n = array.Length;  // длина масива
+int find = 85;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;        // останавливает цикл на первом сопадение     
+}
+    
+    index++;
+}
