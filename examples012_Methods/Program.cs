739c7036342e82d7f684fabfce8ed92aaa90ab18
Mark <masmik@rambler.ru>
2022-08-13 16:14:26 +0300

    add examples012++

--- examples012_Methods/Program.cs
+++ examples012_Methods/Program.cs
@@ -99,3 +99,6 @@ string Replace(string text, char oldValue, char newValue)
 
 string newText = Replace(text, ' ', '|');
 Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
